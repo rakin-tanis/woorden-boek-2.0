255fbd0bda7ac13bc11ed6b88c35ea934aec61d3
Akin <akin.tanis.dev@gmail.com>
2024-11-28 16:05:17 +0100

    joker animation

--- src/components/game/joker/JokerWinAnimation.tsx
+++ src/components/game/joker/JokerWinAnimation.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { motion } from 'framer-motion';
 import { JokerButtonVariantsDetail, JOKERS } from './jokerVariants';
 import React from 'react';
@@ -7,14 +9,15 @@ import { Circle } from 'lucide-react';
 export const JokerWinAnimation: React.FC<{ jokers: { id: string, count: number }[] }> = ({ jokers }) => {
 
   return (
-    <div className="absolute inset-0 bottom-16 top-16 z-50 flex flex-row items-center justify-center gap-4 flex-wrap">
+    <div className="absolute inset-0 bottom-20 top-40 right-6 left-6 z-50 rounded-lg flex flex-row items-center justify-center gap-4 flex-wrap">
       {/* <div className="p-20 rounded-full relative flex flex-row flex-wrap gap-4"> */}
 
       {jokers.map((joker, index) => {
         const newJoker = JOKERS.find(j => j.id === joker.id);
         const variant = newJoker?.variant || 'rose';
         const icon = newJoker?.icon || Circle
-        const colorClass = 'text-' + (JokerButtonVariantsDetail[variant]?.color || 'text-white');
+        const textColor = 'text-' + (JokerButtonVariantsDetail[variant]?.color || 'text-white');
+        const bgClass = JokerButtonVariantsDetail[variant]?.bgColor
         return (
           <motion.div
             key={joker.id}
@@ -32,16 +35,17 @@ export const JokerWinAnimation: React.FC<{ jokers: { id: string, count: number }
               }
             }}
             exit={{ scale: 0, opacity: 0, y: -50, transition: { duration: 0.3 } }}
-            className={`relative flex space-x-4 items-center ${colorClass} text-4xl font-bold font-mono`}
+            className={`relative flex space-x-4 items-center gap-1`}
             style={{
-              textShadow: 'rgba(255,255,0,0.9) 0px 0px 12px',
+              textShadow: 'rgba(0,0,0,0.9) 0px 0px 3px',
             }}>
-            +{joker.count}
+            <div className={`${textColor} text-3xl font-bold font-mono`}>
+              +{joker.count}
+            </div>
             {React.createElement(icon, {
-              className: `${colorClass} w-12 h-12 animate-turnAround`,
+              className: `text-white ${bgClass} p-2 rounded-md w-10 h-12 animate-turnAround`,
               style: {
-                textShadow: 'rgba(255,255,0,0.9) 0px 0px 12px',
-                filter: 'drop-shadow(0 0 12px rgba(255, 255, 0, 0.9))'
+                filter: 'drop-shadow(0 0 4px rgba(0, 0, 0, 0.9))'
               }
             })}
           </motion.div>
